f9d0950d04fe2951b5da67bbf2b414a23737961c
miya0001 <miya@wpist.me>
2014-05-07 14:54:47 +0900

    ver 0.4.0

--- dynamic-hostname.php
+++ dynamic-hostname.php
@@ -3,7 +3,7 @@
  * Plugin Name: Dynamic Hostname
  * Plugin URI:  https://wpist.me/
  * Description: Set hostname dynamically for the development.
- * Version:     0.3.0
+ * Version:     0.4.0
  * Author:      Takayuki Miyauchi
  * Author URI:  https://wpist.me/
  * License:     GPLv2
